7644e2813bb82240dbb768ccf8f04a0cd2a0e731
Ravi Kant Singh <raysk7161@gmail.com>
2020-02-22 14:37:43 +0530

    fixed verbose bug;

--- logs.js
+++ logs.js
@@ -5,7 +5,7 @@ module.exports = {
   warn: (message = 'warn') => console.warn('%cwarning', STYLES.warn, '', message),
   error: (message = 'error') => console.error('%cerror', STYLES.error, '', message),
   debug: (message = 'debug') => console.debug('%cdebug', STYLES.debug, '', message),
-  silly: (message = 'silly') => console.silly('%csilly', STYLES.silly, '', message),
-  verbose: (message = 'verbose') => console.verbose('%cverbose', STYLES.verbose, '', message),
+  silly: (message = 'silly') => console.log('%csilly', STYLES.silly, '', message),
+  verbose: (message = 'verbose') => console.log('%cverbose', STYLES.verbose, '', message),
   custom: (message= 'custom', tag = 'custom', style=STYLES.tag) => console.log(`%c${tag}`, style, '', message),
 }
