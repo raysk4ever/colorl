5a878bb72f434769e913eba70fa81b2c204e7afe
raysk4ever <raysk7161@gmail.com>
2021-12-20 22:07:47 +0530

    bug fixed

--- logs.js
+++ logs.js
@@ -4,7 +4,7 @@ module.exports = {
   info: (message = 'info') => console.info('%cinfo', STYLES.info, '', message),
   warn: (message = 'warn') => console.warn('%cwarning', STYLES.warn, '', message),
   error: (message = 'error') => console.error('%cerror', STYLES.error, '', message),
-  debug: (message = 'debug') => console.debug('%cdebug', STYLES.debug, '', message),
+  debug: (message = 'debug') => console.log('%cdebug', STYLES.debug, '', message),
   silly: (message = 'silly') => console.log('%csilly', STYLES.silly, '', message),
   verbose: (message = 'verbose') => console.log('%cverbose', STYLES.verbose, '', message),
   custom: (message= 'custom', tag = 'custom', style=STYLES.tag) => console.log(`%c${tag}`, style, '', message),
